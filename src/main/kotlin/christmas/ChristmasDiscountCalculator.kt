d04826ff60a873e490d12568acda3bfc32c625b7
seonm9119 <seonm9119@gmail.com>
2023-11-11 07:38:40 +0900

    refactor: modify functions

--- src/main/kotlin/christmas/ChristmasDiscountCalculator.kt
+++ src/main/kotlin/christmas/ChristmasDiscountCalculator.kt
@@ -1,150 +1,64 @@
 package christmas
-
 import java.time.LocalDate
 
-val appetizer = mapOf(
-    "양송이수프" to 6000,
-    "타파스" to 5500,
-    "시저샐러드" to 8000
-)
 
-val mainMenu = mapOf(
 
-    "티본스테이크" to 55000,
-    "바비큐립" to 54000,
-    "해산물파스타" to 35000,
-    "크리스마스파스타" to 25000
+class ChristmasDiscountCalculator(private val day: Int, private val userMenus: Map<String, Int>) {
 
-)
+    private val date = LocalDate.of(LocalDate.now().year, 12, day)
+    private val dayOfWeek = date.dayOfWeek.value
+    private val starDay = listOf(3,10,17,24,31)
 
-val dessert = mapOf(
-
-    "초코케이크" to 15000,
-    "아이스크림" to 5000
-
-)
-
-val drinks = mapOf(
-
-    "제로콜라" to 3000,
-    "레드와인" to 60000,
-    "샴페인" to 25000
-
-)
-
-val menus = mapOf(
-    "appetizer" to appetizer,
-    "mainMenu" to mainMenu,
-    "dessert" to dessert,
-    "drinks" to drinks)
-
-class ChristmasDiscountCalculator(day: Int, userMenus: Map<String, Int>) {
-
-    val day = day
-    val userMenus = userMenus
-    val date = LocalDate.of(LocalDate.now().year, 12, day)
-    val dayOfWeek = date.dayOfWeek.value
     private var totalPrice = 0
-
-
     private var totalBenefit = 0
+    val benefits = Discount.entries.associateWith { 0 }.toMutableMap()
 
-
-
-
-    val benefits = mutableMapOf(
-
-        "크리스마스 디데이 할인" to 0,
-        "평일 할인" to 0,
-        "특별 할인" to 0,
-        "증정 이벤트" to 0,
-
-        )
 
     init {
         calculateTotalPrice()
-        calculateFreeItem()
-        calculateDdayDiscount()
-        calculateWeekDiscount()
-        calculateWeekendDiscount()
-        calculateSpecialDiscount()
         calculateTotalBenefits()
     }
 
-    fun calculateTotalPrice(){
+    private fun calculateTotalPrice() =
+        userMenus.forEach { (menu, quantity) -> totalPrice += Menus.allMenu[menu]!! * quantity }
 
+    private fun calculateFreeItem() =
+        if (totalPrice >= 120000 ) Discount.FREE_ITEM.price else 0
+
+    private fun calculateSpecialDiscount() =
+        if (day in starDay) Discount.SPECIAL.price else 0
+
+    private fun calculateDdayDiscount()
+            = if (day < 26) (day - 1) * Discount.CHRISTMAS_D_DAY.price + 1000 else 0
+
+    private fun calculateWeekOrWeekendDiscount(discount: Discount, category: Menus): Int{
+        var total = 0
         userMenus.forEach { menu, quantity ->
-            val menuCategory = menus.keys.find { menu in menus[it]!! }
+            if (category.menu.keys.contains(menu)) total += discount.price * quantity
+        }
+        return total
+    }
 
-            if (menuCategory != null && menus[menuCategory]!!.containsKey(menu)) {
-                totalPrice += menus[menuCategory]!![menu]!! * quantity.toInt()
-            }
+    private fun calculateTotalBenefits() {
+
+        benefits[Discount.FREE_ITEM] = calculateFreeItem()
+        benefits[Discount.SPECIAL] = calculateSpecialDiscount()
+        benefits[Discount.CHRISTMAS_D_DAY] = calculateDdayDiscount()
+
+        when (dayOfWeek) {
+            5, 6 -> benefits[Discount.WEEKEND] =
+                calculateWeekOrWeekendDiscount(Discount.WEEKEND, Menus.MAIN_MENU)
+            else -> benefits[Discount.WEEKDAY] =
+                calculateWeekOrWeekendDiscount(Discount.WEEKDAY, Menus.DESSERT)
         }
 
+        totalBenefit += benefits.values.sum()
     }
 
-    fun getTotalPrice()=totalPrice
-
-    fun calculateFreeItem(){
-
-        if (totalPrice >= 120000) benefits["증정 이벤트"] = 25000
-
-    }
-
-    fun isFreeItem() = if (benefits["증정 이벤트"]!=0) true else false
-    fun calculateDdayDiscount(){
-
-        if (day < 26){
-            benefits["크리스마스 디데이 할인"] = (day-1)*100 + 1000
-        }
-
-    }
-
-    fun calculateWeekDiscount(){
-
-        val isWeekday = dayOfWeek in 4..7
-
-        var weekDiscount = 0
-        if (isWeekday){
-            userMenus.forEach { menu, quantity ->
-                val menuCategory ="dessert"
-
-                if (menus[menuCategory]!!.containsKey(menu)) {
-                    weekDiscount += 2023 * quantity
-                }
-            }
-            benefits["평일 할인"] = weekDiscount
-        }
-    }
-
-    fun calculateWeekendDiscount(){
-
-        var weekenddiscount = 0
-        val isWeekend = dayOfWeek == 5 || dayOfWeek == 6
-        if (isWeekend){
-            userMenus.forEach { menu, quantity ->
-                val menuCategory ="mainMenu"
-
-                if (menus[menuCategory]!!.containsKey(menu)) {
-                    weekenddiscount += 2023 * quantity
-                }
-            }
-            benefits["주말 할인"] = weekenddiscount
-        }
-
-    }
-
-    fun calculateSpecialDiscount(){
-
-        val starDay = listOf(3,10,17,24,31)
-
-        if (day in starDay) benefits["특별 할인"] = 1000
-
-
-    }
-
-    fun calculateTotalBenefits() = benefits.forEach { _, value -> totalBenefit+=value }
-
+    fun getTotalPrice() = totalPrice
     fun getTotalBenefit() = totalBenefit
+    fun isFreeItem() = benefits[Discount.FREE_ITEM]!=0
+
+
 
 }
