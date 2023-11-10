95fc79e78545382c93dc78f8df0d84c034ad20fb
seonm9119 <seonm9119@gmail.com>
2023-11-11 01:12:52 +0900

    test: add test cases for readMenu and readDate

--- src/test/kotlin/christmas/ApplicationTest.kt
+++ src/test/kotlin/christmas/ApplicationTest.kt
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-
+import org.junit.jupiter.api.Nested
 
 class ApplicationTest : NsTest() {
     @Test
@@ -43,7 +43,7 @@ class ApplicationTest : NsTest() {
     fun `주문 예외 테스트`() {
         assertSimpleTest {
             runException("3", "제로콜라-a")
-            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            assertThat(output()).contains("[ERROR-3] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
         }
     }
 
@@ -54,4 +54,60 @@ class ApplicationTest : NsTest() {
     companion object {
         private val LINE_SEPARATOR = System.lineSeparator()
     }
+
+    //추가 테스트
+    @Nested
+    inner class `날짜 예외 테스트 추가` {
+
+        @Test
+        fun `1 이상 31 이하의 숫자가 아닌 경우`() {
+            assertSimpleTest {
+                runException("45")
+                assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
+            }
+        }
+
+
+    }
+
+    @Nested
+    inner class `주문 예외 테스트 추가` {
+
+        @Test
+        fun `메뉴 형식이 예시와 다른 경우`() {
+            assertSimpleTest {
+                runException("3", "티본스테이크=1,바비큐립=1,초코케이크=2,제로콜라=1")
+                assertThat(output()).contains("[ERROR-3] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            }
+
+        }
+
+        @Test
+        fun `주문한 메뉴의 갯수가 21개 이상일 경우`() {
+            assertSimpleTest {
+                runException("3", "티본스테이크-3,바비큐립-4,초코케이크-8,제로콜라-103")
+                assertThat(output()).contains("[ERROR-6] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            }
+        }
+
+        @Test
+        fun `메뉴의 개수는 1 이상이 아닐 경우`() {
+            assertSimpleTest {
+                runException("3", "티본스테이크-0,바비큐립-0,초코케이크-8")
+                assertThat(output()).contains("[ERROR-2] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            }
+        }
+
+        @Test
+        fun `중복 메뉴를 입력한 경우`() {
+            assertSimpleTest {
+                runException("3", "시저샐러드-1,시저샐러드-1")
+                assertThat(output()).contains("[ERROR-4] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            }
+        }
+
+
+    }
+
+
 }
