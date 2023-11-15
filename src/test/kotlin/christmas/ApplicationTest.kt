577afee91d671f95dfc18348c0fe9c629649884b
seonm9119 <seonm9119@gmail.com>
2023-11-15 20:05:28 +0900

    test: add event test cases

--- src/test/kotlin/christmas/ApplicationTest.kt
+++ src/test/kotlin/christmas/ApplicationTest.kt
@@ -73,21 +73,14 @@ class ApplicationTest : NsTest() {
     @Nested
     inner class `주문 예외 테스트 추가` {
 
+
         @Test
-        fun `메뉴 형식이 예시와 다른 경우`() {
+        fun `고객이 메뉴판에 없는 메뉴를 입력하는 경우`() {
             assertSimpleTest {
-                runException("3", "티본스테이크=1,바비큐립=1,초코케이크=2,제로콜라=1")
+                runException("3", "스프라이트-1")
                 assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
             }
 
-        }
-
-        @Test
-        fun `주문한 메뉴의 갯수가 21개 이상일 경우`() {
-            assertSimpleTest {
-                runException("3", "티본스테이크-3,바비큐립-4,초코케이크-8,제로콜라-103")
-                assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
-            }
         }
 
         @Test
@@ -99,6 +92,15 @@ class ApplicationTest : NsTest() {
         }
 
         @Test
+        fun `메뉴 형식이 예시와 다른 경우`() {
+            assertSimpleTest {
+                runException("3", "티본스테이크=1,바비큐립=1,초코케이크=2,제로콜라=1")
+                assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.")
+            }
+
+        }
+
+        @Test
         fun `중복 메뉴를 입력한 경우`() {
             assertSimpleTest {
                 runException("3", "시저샐러드-1,시저샐러드-1")
@@ -106,8 +108,112 @@ class ApplicationTest : NsTest() {
             }
         }
 
+        @Test
+        fun `음료만 주문 할 경우`() {
+            assertSimpleTest {
+                runException("3", "제로콜라-1,레드와인-3")
+                assertThat(output()).contains("[ERROR] 음료만 주문할 수 없습니다.")
+            }
+        }
+
+        @Test
+        fun `주문한 메뉴의 갯수가 21개 이상일 경우`() {
+            assertSimpleTest {
+                runException("3", "티본스테이크-3,바비큐립-4,제로콜라-103")
+                assertThat(output()).contains("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.")
+            }
+        }
+
+
+    }
+
+    @Nested
+    inner class `이벤트 테스트 추가` {
+
+        @Test
+        fun `총주문 금액 10,000원 이하 혜택 없음`() {
+            assertSimpleTest {
+                run("3", "타파스-1,제로콜라-1")
+                assertThat(output()).contains("<혜택 내역>$LINE_SEPARATOR".toString() + "없음")
+            }
+        }
+
+        @Test
+        fun `증정메뉴 있을경우`() {
+            assertSimpleTest {
+                run("3", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("<증정 메뉴>$LINE_SEPARATOR".toString() + "샴페인 1개")
+            }
+        }
+
+        @Test
+        fun `크리스마스 디데이 할인`() {
+            assertSimpleTest {
+                run("3", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("크리스마스 디데이 할인: -1,200원")
+            }
+        }
+
+        @Test
+        fun `평일할인`() {
+            assertSimpleTest {
+                run("7", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("평일 할인: -2,023원")
+            }
+        }
+
+        @Test
+        fun `주말할인`() {
+            assertSimpleTest {
+                run("15", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("주말 할인: -4,046원")
+            }
+        }
+
+        @Test
+        fun `특별할인`() {
+            assertSimpleTest {
+                run("25", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("특별 할인: -1,000원")
+            }
+        }
+
+
+
+
+
 
     }
 
 
+    @Nested
+    inner class `배지 테스트 추가`{
+
+        @Test
+        fun `별`() {
+            assertSimpleTest {
+                run("17", "레드와인-1,초코케이크-1")
+                assertThat(output()).contains("<12월 이벤트 배지>$LINE_SEPARATOR".toString() + "별")
+            }
+        }
+
+        @Test
+        fun `트리`() {
+            assertSimpleTest {
+                run("24", "초코케이크-5")
+                assertThat(output()).contains("<12월 이벤트 배지>$LINE_SEPARATOR".toString() + "트리")
+            }
+        }
+
+        @Test
+        fun `산타`() {
+            assertSimpleTest {
+                run("25", "해산물파스타-2,레드와인-1,초코케이크-1")
+                assertThat(output()).contains("<12월 이벤트 배지>$LINE_SEPARATOR".toString() + "산타")
+            }
+        }
+
+
+
+    }
 }
